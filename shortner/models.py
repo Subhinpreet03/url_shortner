e13f1c47a0a440d2e9c350e65a41f6c59230358a
Subhipreet03 <subhinpreet03@gmail.com>
2025-01-19 16:24:58 +0530

    initial commit of models and settings.py for loading data from env file

--- shortner/models.py
+++ shortner/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from hashlib import md5
+from datetime import timedelta, datetime
 
-# Create your models here.
+
+class URL(models.Model):
+    original_url = models.URLField(unique=True)
+    short_url = models.CharField(max_length=10, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField()
+    access_count = models.IntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.short_url:
+            # Generate a hash-based short URL
+            self.short_url = md5(self.original_url.encode()).hexdigest()[:10]
+        super().save(*args, **kwargs)
+
+
+class AccessLog(models.Model):
+    url = models.ForeignKey(URL, on_delete=models.CASCADE, related_name="access_logs")
+    accessed_at = models.DateTimeField(auto_now_add=True)
+    ip_address = models.GenericIPAddressField()
--- url_shortner/settings.py
+++ url_shortner/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shortener'
+    'shortner'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +80,12 @@ WSGI_APPLICATION = 'url_shortner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
